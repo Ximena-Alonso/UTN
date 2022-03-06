903be5e5b53f76827b79df201ecd26ee16eae6bc
Ximena Alonso <alonsoxime22@hotmail.com>
2022-03-06 13:53:42 -0300

    estilos

--- MODULO 4/unidad 3/routes/contacto.js
+++ MODULO 4/unidad 3/routes/contacto.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('contacto');
+  res.render('contacto', { title: 'SKY Ascensores' });
 });
 
 module.exports = router;
