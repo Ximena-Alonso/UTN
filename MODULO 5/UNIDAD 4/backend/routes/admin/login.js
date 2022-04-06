08b98efeefcb9f970b10b7fdbcb9d8f065dbe6a0
Ximena Alonso <alonsoxime22@hotmail.com>
2022-04-06 20:22:29 -0300

    sesion

--- MODULO 5/UNIDAD 4/backend/routes/admin/login.js
+++ MODULO 5/UNIDAD 4/backend/routes/admin/login.js
@@ -1,11 +1,34 @@
 var express = require('express');
+const res = require('express/lib/response');
+const async = require('hbs/lib/async');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('admin/login', { 
     layout:'admin/layout' 
   });
+});
+
+router.post('/', async (req, res, next)=>{
+  try {
+    var usuario=req.body.usuario;
+    var password= req.body.password;
+
+    var data=await usuariosModel.getUserByPassword (usuario, password);
+
+    if (data != undefined) {
+      res.redirect('/admin/novedaes');
+    } else {
+      res.render ('/admin/login',{
+        layout: 'admin/layout',
+        error: true
+      });
+    }
+  } catch (error) {
+    console.log (error);
+  }
 });
 
 module.exports = router;
